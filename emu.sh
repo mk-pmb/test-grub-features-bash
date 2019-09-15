bd74c22759123851f2bdc05037795ebdada15f46
mk-pmb <mk-pmb@users.noreply.github.com>
2019-09-15 04:05:11 +0200

    verify_first_input_file

--- emu.sh
+++ emu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # -*- coding: utf-8, tab-width: 2 -*-
 
+
 function grub_emu_run_files () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
   [ "$#" -ge 1 ] || return 4$(echo "E: no input files given!" >&2)
 
-  local LOGFN="$1"
+  local FIRST_INPUT_FILE="$1"; shift
+  verify_first_input_file || return $?
+  local LOGFN="$FIRST_INPUT_FILE"
   LOGFN="${LOGFN%.txt}"
   LOGFN="$LOGFN.log"
 
@@ -22,7 +25,7 @@ function grub_emu_run_files () {
       : mostly_copy
       s~^\xEF\xBB\xBF~~
       p
-    ') -- "$@"
+    ') -- "$FIRST_INPUT_FILE" "$@"
     echo exit
   ) | DISPLAY= grub-emu --directory="$SELFPATH" |& LANG=C sed -urf <(echo '
     s~\x1B\[[0-9;]*[A-Za-z]~~g
@@ -40,5 +43,41 @@ function grub_emu_run_files () {
     ') | tee -- "$LOGFN"
     git add -- "$LOGFN"
 }
+
+
+function verify_first_input_file () {
+  local ORIG="$FIRST_INPUT_FILE"
+  [ -f "$ORIG" ] && return 0
+
+  local MAYBE=
+  for MAYBE in "${ORIG%.}"{,.txt}; do
+    [ -f "$MAYBE" ] || continue
+    FIRST_INPUT_FILE="$MAYBE"
+    return 0
+  done
+
+  echo "E: input file is not a file: $ORIG" >&2
+  return 4
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 grub_emu_run_files "$@"; exit $?

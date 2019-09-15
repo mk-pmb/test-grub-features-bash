b87a88478b30910ab317006f766dfaca8af8b199
mk-pmb <mk-pmb@users.noreply.github.com>
2019-09-15 03:06:26 +0200

    emu tamer

---
+++ emu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+function grub_emu_run_files () {
+  export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
+  local SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
+  [ "$#" -ge 1 ] || return 4$(echo "E: no input files given!" >&2)
+
+  local LOGFN="$1"
+  LOGFN="${LOGFN%.txt}"
+  LOGFN="$LOGFN.log"
+
+  ( LANG=C sed -rf <(echo '
+      /^# -\*- /{n;b skip_blanks}
+      b mostly_copy
+
+      : skip_blanks
+        /./b mostly_copy
+        n
+      b skip_blanks
+
+      : mostly_copy
+      s~^\xEF\xBB\xBF~~
+    ') -- "$@"
+    echo exit
+  ) | DISPLAY= grub-emu --directory="$SELFPATH" |& LANG=C sed -urf <(echo '
+    s~\x1B\[[0-9;]*[A-Za-z]~~g
+    s~^\r~~
+    s~^(grub> ) +~\1~
+    ') | sed -nrf <(echo '
+    1{
+      : skip_intro
+      /\n\n$/!{N;b skip_intro}
+      b
+    }
+    /^grub> $/b
+    /^grub> exit$/q
+    p
+    ') | tee -- "$LOGFN"
+}
+
+grub_emu_run_files "$@"; exit $?
